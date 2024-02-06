1cd28cc2f697dd3f1dde20fe398a77e4ddf0e10c
Matheus Noguerol <matheusnoguerol15@gmail.com>
2024-02-06 15:39:15 -0300

    Diversos ajustes.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,69 @@
-<template>
-  <div id="app">
-    <HeaderComponent />
-
-    <section>
-      <img src="../public/minhaFoto.tiff" alt="">
-    </section>
-
-  </div>
-</template>
 
 <script>
-
 // import HelloWorld from './components/HelloWorld.vue'
-import HeaderComponent from './components/HeaderComponent.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    HeaderComponent,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-image: url('../public/fundo.jpg');
+<template>
+  <div id="app" style="background: linear-gradient(#469cfc, #3f5efb, #469cfc);">
+    
+    <b-container class="">
+      <b-row class="pt-5" id="navBar">
+        <b-col lg="2">
+          
+        </b-col>
+
+        <b-col lg="2"></b-col>
+
+        <b-col lg="1"><b-button variant="transparent">About</b-button></b-col>
+        <b-col lg="1"><b-button variant="transparent">Journey</b-button></b-col>
+        <b-col lg="1"><b-button variant="transparent">Projects</b-button></b-col>
+        <b-col lg="1"><b-button variant="transparent">Contact</b-button></b-col>
+      </b-row>
+      
+      <section>
+        <hr class="hr-custom">
+        <b-row>
+          <b-col lg="7">
+            <b-row>
+              <b-col lg="12" id="texto">
+                <h3><b>Olá, meu nome é <span id="nome">Matheus Noguerol!</span></b></h3>
+              </b-col>
+            </b-row>
+
+            <b-row>
+              <b-col lang="12">
+                <img class="img-fluid mx-auto rounded" src="../public/fullstack.png" id="Sistema Total On" alt="Parceria Total+" title="Parceria Total+">
+              </b-col>
+            </b-row>
+          </b-col>
+
+          <b-col lg="5" class="my-10">
+            <img class="img-fluid rounded" style="transform: scale(1.7);" src="../public/minhaFoto2.png" id="Sistema Total On" alt="Parceria Total+" title="Parceria Total+">
+          </b-col>
+        </b-row>
+      </section>
+    </b-container>
+    
+  </div>
+</template>
+
+<style scoped>
+#navBar{
+  padding-right: 0;
+  margin-right: 0;
+}
+#texto{
+  padding-top: 2rem;
+}
+#nome{
+  text-transform: uppercase;
+  text-decoration: underline;
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,29 +1,29 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg" id="nav">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-        </div>
+     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgba(12, 12, 12, 0.696);">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Logo</a>
+      <!-- Botão de alternância para dispositivos pequenos -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <!-- Itens da barra de navegação -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <!-- Adicione mais itens conforme necessário -->
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
   </div>
 </template>
 
@@ -37,5 +37,6 @@ export default {
 <style scoped>
   #nav{
     background: transparent;
+    background-color: rgba(12, 12, 12, 0.696);
   }
 </style>
