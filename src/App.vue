9418a6e96d1298f267f19c2bfc6a5e7ea9e04516
Matheus Noguerol <matheusnoguerol15@gmail.com>
2024-02-15 13:31:20 -0300

    implementacao das minhas tech.

--- src/App.vue
+++ src/App.vue
@@ -27,25 +27,37 @@ export default {
         <b-col lg="1"><b-button variant="transparent">Contact</b-button></b-col>
       </b-row>
       
-      <section>
+      <section id="about">
         <hr class="hr-custom">
         <b-row>
-          <b-col lg="7">
-            <b-row>
-              <b-col lg="12" id="texto">
-                <h3><b>Olá, meu nome é <span id="nome">Matheus Noguerol!</span></b></h3>
-              </b-col>
-            </b-row>
-
-            <b-row>
-              <b-col lang="12">
-                <img class="img-fluid mx-auto rounded" src="../public/fullstack.png" id="Sistema Total On" alt="Parceria Total+" title="Parceria Total+">
-              </b-col>
-            </b-row>
+          <b-col lg="6" id="apresentacao" class="d-flex align-items-center justify-content-center">
+            <div>
+              <b-row>
+                <b-col lg="12">
+                  <h1 id="saudacao">OLÁ,</h1>
+                  <p><b>Me chamo <span id="nome">Matheus Noguerol!</span></b></p>
+                  <p class="ocupation">FULL STACK web DEVELOPER</p>
+                </b-col>
+              </b-row>
+              <b-row>
+                <b-col lg="12">
+                  <div id="container-technology" class="bg-white rounded"> 
+                    <img class="technology" src="../public/html.png" title="HTML5">  
+                    <img class="technology" src="../public/css.png" title="CSS 3">
+                    <img class="technology" src="../public/bootstrap.png" title="BOOTSTRAP 5">
+                    <img class="technology" src="../public/js.png" title="JAVASCRIPT">
+                    <img class="technology" src="../public/vue.png" title="VUEJS">
+                    <img class="technology" src="../public/PHP.png" title="PHP">
+                    <img class="technology" src="../public/laravel.png" title="LARAVEL">
+                    <img class="technology" src="../public/mysql.png" title="MYSQL">
+                  </div>
+                </b-col>
+              </b-row>
+            </div>
           </b-col>
 
-          <b-col lg="5" class="my-10">
-            <img class="img-fluid rounded" style="transform: scale(1.7);" src="../public/minhaFoto2.png" id="Sistema Total On" alt="Parceria Total+" title="Parceria Total+">
+          <b-col lg="6" style="padding-top: 100px;">
+            <img class="img-fluid rounded" style="transform: scale(1.7); height: 200px;" src="../public/minhaFoto2.png" id="Sistema Total On" alt="Parceria Total+" title="Parceria Total+">
           </b-col>
         </b-row>
       </section>
@@ -55,15 +67,57 @@ export default {
 </template>
 
 <style scoped>
+#app{
+  height: 100vh;
+}
 #navBar{
   padding-right: 0;
   margin-right: 0;
 }
+
 #texto{
   padding-top: 2rem;
 }
+
 #nome{
   text-transform: uppercase;
   text-decoration: underline;
 }
+
+section #saudacao{
+  font-family: "Protest Strike", sans-serif;
+  color: white;
+  font-style: normal;
+  animation: slideIn 2s ease forwards;
+}
+
+@keyframes slideIn {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+.ocupation {
+  font-family: "Protest Strike", sans-serif;
+  font-weight: 400;
+  font-style: bolder;
+  color: white;
+  font-size: 3rem;
+}
+
+.technology {
+  height: 7rem;
+}
+
+div#container-technology{
+  display: flex;
+  flex-direction:row;
+  flex-wrap: wrap;
+} 
+
+
+/* section#about */
 </style>
